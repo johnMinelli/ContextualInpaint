a14b32e1f5d83ed80bc540cb5ef234db5f14de40
Giovanni Minelli <giovanni.minelli3@gmail.com>
2024-09-30 10:46:18 +0200

    llava compatibility update for transformers library

--- preprocess/llava_predictor.py
+++ preprocess/llava_predictor.py
@@ -104,11 +104,7 @@ class Predictor(BasePredictor):
                 num_beams=1, max_new_tokens=max_tokens, use_cache=True,
                 stopping_criteria=[stopping_criteria], )
 
-        input_token_len = input_ids.shape[1]
-        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
-        if n_diff_input_output > 0:
-            print(f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids")
-        outputs = self.tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        outputs = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
         outputs = outputs.strip()
         if outputs.endswith(stop_str):
             outputs = outputs[: -len(stop_str)]
